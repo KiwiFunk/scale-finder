1105c93f9ff5184431ae106e9ca296fb6d33da1e
Alex <alex_p97@live.com>
2025-01-17 22:34:26 +0000

    Updated logic and created a loop to handle major or minor scales for root note

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿//Array to hold notes
 string[] notes = new string[] { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
 
+string? returnResult = null;
 
 Console.WriteLine("Enter the root note: ");
 string rootNote = Console.ReadLine().ToUpper();
@@ -25,12 +26,44 @@ int[] minorIntervals = new int[] { 2, 1, 2, 2, 1, 2, 2 };
 string[] scale = new string[8];                                 //7 Notes plus the Octave
 scale[0] = notes[index];                                        //Root note
 
-for (int i = 1; i < 8; i++)                                     //Start from 1 as we have already set the root note
-{
-    index = (index + majorIntervals[i - 1]) % notes.Length;     //Calculate the index of the next note
-    scale[i] = notes[index];                                    //Set the next note in the scale
-}
 
-//Output the scale to the console
-Console.WriteLine($"The {rootNote} Major Scale is: ");
-Console.WriteLine(string.Join(" ", scale));
+bool validInput = false;
+do
+{
+    Console.WriteLine("Major or Minor scale?");
+    returnResult = Console.ReadLine();
+    if (returnResult != null)
+    {
+        if (returnResult.ToLower().Trim() == "major")
+        {
+            for (int i = 1; i < 8; i++)                                     //Start from 1 as we have already set the root note
+            {
+                index = (index + majorIntervals[i - 1]) % notes.Length;     //Use modulus to avoid going out of bounds
+                scale[i] = notes[index];                                    //Set the next note in the scale
+            }
+            Console.WriteLine($"The {rootNote} Major Scale is: ");
+            Console.WriteLine(string.Join(" ", scale));
+            validInput = true;
+        }
+
+        else if (returnResult.ToLower().Trim() == "minor")
+        {
+            for (int i = 1; i < 8; i++)                                     
+            {
+                index = (index + minorIntervals[i - 1]) % notes.Length;     
+                scale[i] = notes[index];                                    
+            }
+            Console.WriteLine($"The {rootNote} Minor Scale is: ");
+            Console.WriteLine(string.Join(" ", scale));
+            validInput = true;
+        }
+
+        else Console.WriteLine("Please enter a valid input");
+    }
+
+} while (!validInput);                                                      //Loop until valid string is entered.
+
+
+/*
+    Add logic/method to handle sharps and flats.
+*/
