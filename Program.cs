07fe0e71f5db6e6d4f1fb45941b31cb3dc575251
Alex <alex_p97@live.com>
2025-01-17 21:58:45 +0000

    Created basic program structure, added console input prompt

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿//Array to hold notes
 string[] notes = new string[] { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+
+
+Console.WriteLine("Enter the root note: ");
+string rootNote = Console.ReadLine().ToUpper();
+
+//Find index of root note
+int index = Array.IndexOf(notes, rootNote);
+
+//Make sure valid note was entered 
+if (index == -1)
+{
+    Console.WriteLine("Invalid note entered");
+    return;
+}
+
+// Major Scale intervals: W, W, H, W, W, W, H
+
+//Minor Scale intervals: W, H, W, W, H, W, W
+
+//Create Scale starting from root note
+
+//Output the scale to the console
